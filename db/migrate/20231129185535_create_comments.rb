3ddfccf1822988c564afcef9b9c9c04c627d2f72
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-11-30 00:04:32 +0500

    Amend comments and likes table

--- db/migrate/20231129185535_create_comments.rb
+++ db/migrate/20231129185535_create_comments.rb
@@ -1,7 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-
+      t.references :user, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
       t.timestamps
     end
   end

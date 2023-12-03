d3b94aac02f0c5a94a10419b7c80289ea02da208
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-12-03 16:56:36 +0500

    Add spec tests to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { User.new(name: "Abu", posts_counter: 0) }
   it 'is valid with a name and a valid posts_counter' do
-    user = User.new(name: 'Abu', posts_counter: 0)
+    #user = User.new(name: 'Abu', posts_counter: 0)
     expect(user).to be_valid
   end
 
   it 'is invalid without a name' do
     user = User.new(posts_counter: 0)
     expect(user).not_to be_valid
+  end
+
+  it 'posts_counter should be integer' do
+    user = User.new(posts_counter: 0)
+    expect(user).to_not be_valid
   end
 end

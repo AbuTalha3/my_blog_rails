65cebea5447c2b3e26376bd8c674237664b0d918
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-12-06 00:02:52 +0500

    Add spec tests for user view and users post view

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+  describe "GET /index" do
+    it "should be succesful response" do
+      get '/users/id/posts'
+      expect(response).to have_http_status(200)
+    end
+
+    it "should render the posts index file" do
+      get '/users/id/posts'
+      expect(response).to render_template(:index)
+    end
+
+    it "should include the placeholder" do
+      get '/users/id/posts'
+      expect(response.body).to include('Here is the posts index page!')
+    end
+  end
+
+  describe 'GET /show' do
+    it 'should be a successfull response' do
+      get '/users/:id/posts/:id'
+      expect(response).to be_successful
+    end
+
+    it 'should render the posts show file' do
+      get '/users/:id/posts/:id'
+      expect(response).to render_template('posts/show')
+    end
+
+    it 'should include the placeholder' do
+      get '/users/:id/posts/:id'
+      expect(response.body).to include('Here is the posts show page!')
+    end
+  end
+end

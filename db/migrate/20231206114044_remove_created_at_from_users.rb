6e7dbafb285fecdadcdd427019c616e82ce1328d
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-12-08 07:03:50 +0500

    Amend required changes

--- db/migrate/20231206114044_remove_created_at_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCreatedAtFromUsers < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :users, :createdAt    
-  end
-end
--- db/migrate/20231206114641_remove_updated_at_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUpdatedAtFromUsers < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :users, :updatedAt
-  end
-end

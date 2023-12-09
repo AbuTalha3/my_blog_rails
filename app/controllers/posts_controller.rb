70b3716d2c92446c11314655215fd65837831668
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-12-10 01:48:51 +0500

    Linters error removed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :text, :user_id)
+    params.require(:post).permit(:title, :text, :author_id)
   end
 end

6a6ee854d56e6aa823481985ab397b3bc5c77dde
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-12-10 01:03:09 +0500

    Amend migration files change

--- db/migrate/20231129185457_create_users.rb
+++ db/migrate/20231129185457_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[7.1]
       t.string :name
       t.string :photo
       t.string :bio
-      t.integer :post_counter
+      t.integer :post_counter, default: 0
 
       t.timestamps
     end
--- db/migrate/20231129185521_create_posts.rb
+++ db/migrate/20231129185521_create_posts.rb
@@ -3,8 +3,8 @@ class CreatePosts < ActiveRecord::Migration[7.1]
     create_table :posts do |t|
       t.string :title
       t.string :text
-      t.integer :comments_counter
-      t.integer :likes_counter
+      t.integer :comments_counter, default: 0
+      t.integer :likes_counter, default: 0
       t.references :author, foreign_key: { to_table: 'users' } 
 
       t.timestamps

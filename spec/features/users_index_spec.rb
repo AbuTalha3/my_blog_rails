f7130d7af99a445decd6463d1788b1c8ebb78af6
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-12-17 03:03:01 +0500

    Add first requirement

--- spec/features/users_index_spec.rb
+++ spec/features/users_index_spec.rb
@@ -25,6 +25,13 @@ RSpec.describe 'User Index', type: :feature do
       expect(page).to have_css("img[src*='https://unsplash.com/photos/3.jpg']")
     end
 
+    it 'should display the number of posts each user has written' do
+      visit users_path
+      expect(page).to have_content("Posts: 3", count: 0)
+      expect(page).to have_content("Posts: 5", count: 0)
+      expect(page).to have_content("Posts: 7", count: 0)
+    end
+
     it 'should redirect to that users show page when clicking on the username' do
       visit users_path
       click_link @user1.name
